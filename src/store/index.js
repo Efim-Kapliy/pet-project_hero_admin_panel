20f2f91671115a2da7730ccb070e76d479917981
Efim-Kapliy <efim0424@mail.ru>
2024-03-13 20:10:56 +0300

    feat: add enhancer and compose in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,26 @@
-import { createStore } from "redux";
-import { combineReducers } from "@reduxjs/toolkit";
+import { createStore, combineReducers, compose } from "redux";
 
 import heroes from "../reducers/heroes";
 import filters from "../reducers/filters";
 
+const enhancer =
+  (createStore) =>
+  (...arg) => {
+    const store = createStore(...arg);
+
+    const oldDispatch = store.dispatch;
+    store.dispatch = (action) => {
+      if (typeof action === "string") {
+        return oldDispatch({ type: action });
+      }
+      return oldDispatch(action);
+    };
+    return store;
+  };
+
 const store = createStore(
   combineReducers({ heroes, filters }),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(enhancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
 
 export default store;
