d5eccaf146b56852ba1c11626cbb0e11fc85af4a
Efim-Kapliy <efim0424@mail.ru>
2024-03-19 18:04:41 +0300

    refactor: replace actions and reducer recording format for heroes from Redux to Redux Toolkit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
+import { createAction } from "@reduxjs/toolkit";
+
 export const fetchHeroes = (request) => (dispatch) => {
   dispatch("HEROES_FETCHING");
-  // dispatch(heroesFetching());
+  dispatch(heroesFetching());
   request("http://localhost:3001/heroes")
     .then((data) => dispatch(heroesFetched(data)))
     .catch(() => dispatch(heroesFetchingError()));
@@ -13,24 +15,30 @@ export const fetchFilteredHeroes = (request) => (dispatch) => {
     .catch(() => dispatch(filtersFetchingError()));
 };
 
-export const heroesFetching = () => {
-  return {
-    type: "HEROES_FETCHING",
-  };
-};
+// export const heroesFetching = () => {
+//   return {
+//     type: "HEROES_FETCHING",
+//   };
+// };
 
-export const heroesFetched = (heroes) => {
-  return {
-    type: "HEROES_FETCHED",
-    payload: heroes,
-  };
-};
+export const heroesFetching = createAction("HEROES_FETCHING");
 
-export const heroesFetchingError = () => {
-  return {
-    type: "HEROES_FETCHING_ERROR",
-  };
-};
+// export const heroesFetched = (heroes) => {
+//   return {
+//     type: "HEROES_FETCHED",
+//     payload: heroes,
+//   };
+// };
+
+export const heroesFetched = createAction("HEROES_FETCHED");
+
+// export const heroesFetchingError = () => {
+//   return {
+//     type: "HEROES_FETCHING_ERROR",
+//   };
+// };
+
+export const heroesFetchingError = createAction("HEROES_FETCHING_ERROR");
 
 export const filtersFetching = () => {
   return {
@@ -67,16 +75,20 @@ export const activeFilterChanged = (filter) => {
 //   }, 1000);
 // };
 
-export const heroCreated = (hero) => {
-  return {
-    type: "HERO_CREATED",
-    payload: hero,
-  };
-};
+// export const heroCreated = (hero) => {
+//   return {
+//     type: "HERO_CREATED",
+//     payload: hero,
+//   };
+// };
 
-export const heroDeleted = (id) => {
-  return {
-    type: "HERO_DELETED",
-    payload: id,
-  };
-};
+export const heroCreated = createAction("HERO_CREATED");
+
+// export const heroDeleted = (id) => {
+//   return {
+//     type: "HERO_DELETED",
+//     payload: id,
+//   };
+// };
+
+export const heroDeleted = createAction("HERO_DELETED");
